1fe65bcd49e8f4d586fd1afcad91d742687968d9
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-08-28 18:27:14 +0200

    create fetch and filter function

--- vue-vite/src/store.js
+++ vue-vite/src/store.js
@@ -2,11 +2,27 @@ import axios from "axios";
 import { reactive } from "vue";
 
 export const store = reactive({
-  
+  filmsList: [],
+  searchText: "",
 });
+//title, original_title, original_language, vote_average
 
-export function filterFilms() {
-  console.log("test");
+export function filterFilms(searchText) {
+  store.filmsList = [];
+
+  fetchFilms(
+    `https://api.themoviedb.org/3/discover/movie?api_key=5b12a2b80ff5a258c5bf9900c0ad169d&${searchText}`
+  );
+  console.log("test2");
 };
+
+export function fetchFilms(nextUrl) {
+  const url = nextUrl ?? "https://api.themoviedb.org/3/discover/movie?api_key=5b12a2b80ff5a258c5bf9900c0ad169d";
+
+  axios
+    .get(url).then((response) => {
+      store.cards = response.data.title;
+    });
+}
 
 //5b12a2b80ff5a258c5bf9900c0ad169d
